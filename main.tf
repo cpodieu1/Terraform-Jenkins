baffc0f488f4652177bdba6ceb607fa9641d9a8f
chris1 <60787119+cpodieu1@users.noreply.github.com>
2020-08-08 21:49:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "maliano"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
